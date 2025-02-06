5a7ae2efd46eb92e52c72e3f12db5ce314dc6350
Terraah <102477867+Terraah@users.noreply.github.com>
2025-02-06 13:50:48 +0100

    Update Code.tf

--- Code.tf
+++ Code.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "~> 2.9.11"
+    }
+  }
+}
+
 provider "proxmox" {
   pm_api_url      = "https://TON_IP_PROXMOX:8006/api2/json"
   pm_user         = "root@pam"
