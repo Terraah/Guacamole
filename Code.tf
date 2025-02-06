fb9b5301d4ed8db81472d21a9c7351e2bce1bbdc
Terraah <102477867+Terraah@users.noreply.github.com>
2025-02-06 13:40:53 +0100

    Update Code.tf

--- Code.tf
+++ Code.tf
@@ -1,40 +1,14 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = "~> 2.9.11"
-    }
-  }
-}
-
 provider "proxmox" {
-  pm_api_url      = "https://your-proxmox-server:8006/api2/json"
-  pm_user         = "root@pve"
-  pm_password     = "azerty"
+  pm_api_url      = "https://TON_IP_PROXMOX:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = var.pm_password
   pm_tls_insecure = true
-}
-
-variable "ci_user" {
-  description = "Nom d'utilisateur pour Cloud-Init"
-  type        = string
-  default     = "bryan"
-}
-
-variable "ci_mdp" {
-  description = "Mot de passe pour Cloud-Init"
-  type        = string
-  sensitive   = true
-}
-
-variable "ssh_key_pub" {
-  description = "Clé publique SSH pour l'accès"
-  type        = string
 }
 
 resource "proxmox_vm_qemu" "guaca" {
   name        = "guacamole"
   agent       = 1
-  target_node = "horizon" 
+  target_node = "horizon"
   clone       = "debian.template"
   scsihw      = "virtio-scsi-pci"
   full_clone  = true
@@ -47,40 +21,33 @@ resource "proxmox_vm_qemu" "guaca" {
   os_type     = "cloud-init"
   tags        = "guacamole"
   pool        = "zone-relais"
-  
+
   # Boot
   bootdisk    = "scsi0"
 
-  # Disque principal
+  # Disque principal (correction ici)
   disk {
-    scsi {
-      scsi0 {
-        disk {
-          size    = "20G"
-          storage = "production"
-          format  = "raw"
-        }
-      }
-    }
-    ide {
-      ide2 {
-        cloudinit {
-          storage = "production"
-        }
-      }
-    }
+    type     = "scsi"
+    storage  = "production"
+    size     = "20G"
+    format   = "raw"
+  }
+
+  disk {
+    type    = "cloud-init"
+    storage = "production"
   }
 
   # Configuration réseau
   network {
-    model   = "virtio"
-    bridge  = "vmbr0"
+    model  = "virtio"
+    bridge = "vmbr0"
   }
 
   network {
-    model   = "virtio"
-    bridge  = "vmbr2"
-    tag     = "20"
+    model  = "virtio"
+    bridge = "vmbr2"
+    tag    = 20
   }
 
   # Cloud-Init pour IP, DNS et accès SSH
@@ -90,20 +57,9 @@ resource "proxmox_vm_qemu" "guaca" {
 
   ipconfig0  = "ip=172.16.0.50/24,gw=172.16.0.14"
   ipconfig1  = "ip=172.16.20.50/27,gw=172.16.20.30"
-  
+
   searchdomain = "mynet.net"
   nameserver   = "172.16.20.29"
 
-  # Script de provisionnement
-  provisioner "remote-exec" {
-    inline = [
-      "#!/bin/bash",
-      "apt update && apt install -y git docker.io docker-compose",
-      "systemctl enable --now docker",
-      "git clone https://github.com/TON_GITHUB/guacamole-setup /opt/guacamole",
-      "cd /opt/guacamole && docker-compose up -d"
-    ]
-  }
-
-  depends_on = [proxmox_vm_qemu.reverse]
+  # Suppression de depends_on sur proxmox_vm_qemu.reverse qui n'existe pas
 }
